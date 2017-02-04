ce07c0055f03355d3bb8058863e49c9a076859ed
tonyardolino <tonya@netcon.com>
2017-02-04 09:17:56 -0500

    Initial Jaart360 version

---
+++ FoodTracker/FoodTracker/MealTableViewController.swift
@@ -0,0 +1,325 @@
+//
+//  MealTableViewController.swift
+//  FoodTracker
+//
+//  Created by Jane Appleseed on 11/15/16.
+//  Copyright © 2016 Apple Inc. All rights reserved.
+//
+
+import UIKit
+import os.log
+import CloudKit
+
+class MealTableViewController: UITableViewController {
+    
+    //MARK: Properties
+    
+    var meals = [Meal]()
+    
+    override func viewDidLoad() {
+        super.viewDidLoad()
+        
+        // Use the edit button item provided by the table view controller.
+        navigationItem.leftBarButtonItem = editButtonItem
+        
+        // Load any saved meals, otherwise load sample data.
+        if let savedMeals = loadMeals() {
+            meals += savedMeals
+        }
+        else {
+            // Load the sample data.
+            //loadSampleMeals()
+            //print("viewDidLoad calling loadCloudMeals()")
+            loadiCloudMeals()
+            //saveMeals()
+            /*
+             print("before \(meals.count)")
+             meals = [Meal]()
+             print("after \(meals.count)")
+             if let savedMeals = loadMeals() {
+             meals += savedMeals
+             print("loadMeals \(meals.count)")
+             } else {
+             print("viewDidLoad loadMeals failed")
+             }
+             */
+        }
+    }
+    
+    override func didReceiveMemoryWarning() {
+        super.didReceiveMemoryWarning()
+        print("didReceiveMemoryWarning")
+        // Dispose of any resources that can be recreated.
+    }
+    
+    //MARK: - Table view data source
+    
+    override func numberOfSections(in tableView: UITableView) -> Int {
+        return 1
+    }
+    
+    override func tableView(_ tableView: UITableView, numberOfRowsInSection section: Int) -> Int {
+        print("numberOfRowsInSection \(meals.count)")
+        return meals.count
+    }
+    
+    
+    override func tableView(_ tableView: UITableView, cellForRowAt indexPath: IndexPath) -> UITableViewCell {
+        
+        // Table view cells are reused and should be dequeued using a cell identifier.
+        let cellIdentifier = "MealTableViewCell"
+        
+        guard let cell = tableView.dequeueReusableCell(withIdentifier: cellIdentifier, for: indexPath) as? MealTableViewCell  else {
+            fatalError("The dequeued cell is not an instance of MealTableViewCell.")
+        }
+        
+        // Fetches the appropriate meal for the data source layout.
+        let meal = meals[indexPath.row]
+        
+        cell.nameLabel.text = meal.name
+        cell.photoImageView.image = meal.photo
+        cell.ratingControl.rating = meal.rating
+        
+        return cell
+    }
+    
+    
+    
+    // Override to support conditional editing of the table view.
+    override func tableView(_ tableView: UITableView, canEditRowAt indexPath: IndexPath) -> Bool {
+        // Return false if you do not want the specified item to be editable.
+        return true
+    }
+    
+    
+    
+    // Override to support editing the table view.
+    override func tableView(_ tableView: UITableView, commit editingStyle: UITableViewCellEditingStyle, forRowAt indexPath: IndexPath) {
+        if editingStyle == .delete {
+            // Delete the row from the data source
+            meals.remove(at: indexPath.row)
+            saveMeals()
+            tableView.deleteRows(at: [indexPath], with: .fade)
+        } else if editingStyle == .insert {
+            // Create a new instance of the appropriate class, insert it into the array, and add a new row to the table view
+        }
+    }
+    
+    
+    /*
+     // Override to support rearranging the table view.
+     override func tableView(_ tableView: UITableView, moveRowAt fromIndexPath: IndexPath, to: IndexPath) {
+     
+     }
+     */
+    
+    /*
+     // Override to support conditional rearranging of the table view.
+     override func tableView(_ tableView: UITableView, canMoveRowAt indexPath: IndexPath) -> Bool {
+     // Return false if you do not want the item to be re-orderable.
+     return true
+     }
+     */
+    
+    
+    //MARK: - Navigation
+    
+    // In a storyboard-based application, you will often want to do a little preparation before navigation
+    override func prepare(for segue: UIStoryboardSegue, sender: Any?) {
+        
+        super.prepare(for: segue, sender: sender)
+        
+        switch(segue.identifier ?? "") {
+            
+        case "AddItem":
+            os_log("Adding a new meal.", log: OSLog.default, type: .debug)
+            
+        case "ShowDetail":
+            guard let mealDetailViewController = segue.destination as? MealViewController else {
+                fatalError("Unexpected destination: \(segue.destination)")
+            }
+            
+            guard let selectedMealCell = sender as? MealTableViewCell else {
+                fatalError("Unexpected sender: \(sender)")
+            }
+            
+            guard let indexPath = tableView.indexPath(for: selectedMealCell) else {
+                fatalError("The selected cell is not being displayed by the table")
+            }
+            
+            let selectedMeal = meals[indexPath.row]
+            mealDetailViewController.meal = selectedMeal
+            
+        default:
+            fatalError("Unexpected Segue Identifier; \(segue.identifier)")
+        }
+    }
+    
+    
+    //MARK: Actions
+    
+    @IBAction func unwindToMealList(sender: UIStoryboardSegue) {
+        if let sourceViewController = sender.source as? MealViewController, let meal = sourceViewController.meal {
+            
+            if let selectedIndexPath = tableView.indexPathForSelectedRow {
+                // Update an existing meal.
+                meals[selectedIndexPath.row] = meal
+                tableView.reloadRows(at: [selectedIndexPath], with: .none)
+            }
+            else {
+                // Add a new meal.
+                let newIndexPath = IndexPath(row: meals.count, section: 0)
+                
+                meals.append(meal)
+                tableView.insertRows(at: [newIndexPath], with: .automatic)
+            }
+            
+            // Save the meals.
+            saveMeals()
+        }
+    }
+    
+    //MARK: Private Methods
+    
+    private func loadSampleMeals() {
+        
+        let photo1 = UIImage(named: "meal1")
+        let photo2 = UIImage(named: "meal2")
+        let photo3 = UIImage(named: "meal3")
+        
+        guard let meal1 = Meal(name: "Caprese Salad", photo: photo1, rating: 4, size: "24x28") else {
+            fatalError("Unable to instantiate meal1")
+        }
+        
+        guard let meal2 = Meal(name: "Chicken and Potatoes", photo: photo2, rating: 5, size: "24x28") else {
+            fatalError("Unable to instantiate meal2")
+        }
+        
+        guard let meal3 = Meal(name: "Pasta with Meatballs", photo: photo3, rating: 3, size: "24x28") else {
+            fatalError("Unable to instantiate meal2")
+        }
+        
+        meals += [meal1, meal2, meal3]
+    }
+    
+    private func saveMeals() {
+        print("saveMeals() called")
+        let isSuccessfulSave = NSKeyedArchiver.archiveRootObject(meals, toFile: Meal.ArchiveURL.path)
+        print("saveMeals completed \(isSuccessfulSave)")
+        if isSuccessfulSave {
+            print("Meals successfully saved.")
+            //os_log("Meals successfully saved.", log: OSLog.default, type: .debug)
+        } else {
+            print("Failed to save meals...")
+            //os_log("Failed to save meals...", log: OSLog.default, type: .error)
+            
+        }
+        /*
+         let name = meals.last?.name
+         let rating = meals.last?.rating
+         let Image: UIImage = (meals.last?.photo)!
+         
+         
+         let DocumentDirectory = FileManager().urls(for: .documentDirectory, in: .userDomainMask).first!
+         let tempURL = DocumentDirectory.appendingPathComponent("temp")
+         let data = UIImageJPEGRepresentation(Image, 10)!
+         do {
+         try data.write(to: tempURL)
+         }
+         catch {
+         os_log("Failed to save temp...", log: OSLog.default, type: .error)
+         }
+         let asset = CKAsset(fileURL: tempURL)
+         
+         
+         let artworkRecordID: CKRecordID = CKRecordID.init(recordName: name!)
+         let artworkRecord: CKRecord = CKRecord.init(recordType: "Artwork", recordID: artworkRecordID)
+         artworkRecord["title"] = name! as CKRecordValue
+         artworkRecord["artist"] = "Jennifer Ardolino" as CKRecordValue
+         artworkRecord["address"] =  "Homosassa, Florida" as CKRecordValue
+         artworkRecord["rating"] = rating! as CKRecordValue
+         artworkRecord["image"] = asset
+         
+         let myContainer: CKContainer = CKContainer.default()
+         let publicDatabase: CKDatabase = myContainer.publicCloudDatabase
+         
+         publicDatabase.save(artworkRecord) { savedRecord, error in
+         }
+         */
+        
+    }
+    
+    private func loadMeals() -> [Meal]?  {
+        return NSKeyedUnarchiver.unarchiveObject(withFile: Meal.ArchiveURL.path) as? [Meal]
+    }
+    private func loadiCloudMeals() {
+        
+        let myContainer: CKContainer = CKContainer.default()
+        let publicDatabase: CKDatabase = myContainer.publicCloudDatabase
+        let mypredicate = NSPredicate.init(value: true)
+        //let size = "24x28"
+        //let mypredicate = NSPredicate(format: "Size == %@", size)
+        let myquery: CKQuery = CKQuery.init(recordType: "Artwork", predicate: mypredicate)
+        //let myquery: CKQuery = CKQuery.init(recordType: "Art", predicate: mypredicate)
+        
+        
+        publicDatabase.perform(myquery, inZoneWith:  nil, completionHandler: ({results, error in
+            
+            if(error != nil) {
+                print("loadiCloudMeals iCLoud Query Failed... error: \(error)")
+                os_log("iCLoud Query Failed...", log: OSLog.default, type: .error)
+            } else {
+                //var myMeal = [Meal]()
+                //print("loadiCloudMeals iCLoud Query sucessful...")
+                //os_log("iCLoud Query successfully.", log: OSLog.default, type: .debug)
+                //print(results!.count)
+                //var i: Int = 0
+                for entry in results! {
+                    //i += 1
+                    //if (i > 10) {
+                    //    break
+                    //}
+                    //print("recordID: \(entry.recordID)")
+                    let name = entry["title"] as? String
+                    let rating = entry["rating"] as? Int
+                    
+                    
+                    guard let asset = entry["image"] as? CKAsset else {
+                        print("Image missing from record")
+                        return
+                    }
+                    guard let imageData = NSData(contentsOf: asset.fileURL) else {
+                        print("Invalid Image")
+                        return
+                    }
+                    let photo  = UIImage(data: imageData as Data)
+                    //print("loadCloudMeals Name: \(name) Size: \(photo?.size)")
+                    let size = entry["Size"] as? String
+                    guard let icloudMeal = Meal(name: name!, photo: photo!, rating: rating!, size: size!)
+                        else {
+                            fatalError("Meals failed to init loadiCloudMeals")
+                    }
+                    self.meals += [icloudMeal]
+                    
+                    //print(" title \(name) \(rating) \(Size)")
+                }
+                /*
+                 print("loadiCLoudMeal calling saveMeals()")
+                 self.saveMeals()
+                 print("before \(self.meals.count)")
+                 self.meals = [Meal]()
+                 print("after \(self.meals.count)")
+                 if let savedMeals = self.loadMeals() {
+                 self.meals += savedMeals
+                 print("loadMeals \(self.meals.count)")
+                 } else {
+                 print("loadMeals failed")
+                 }
+                 */
+                self.tableView.reloadData()
+            }
+        }))
+        //return NSKeyedUnarchiver.unarchiveObject(withFile: Meal.ArchiveURL.path) as? [Meal]
+    }
+    
+}
---
+++ FoodTracker/FoodTracker/MealViewController.swift
@@ -0,0 +1,289 @@
+//
+//  MealViewController.swift
+//  FoodTracker
+//
+//  Created by Jane Appleseed on 10/17/16.
+//  Copyright © 2016 Apple Inc. All rights reserved.
+//
+
+import UIKit
+import os.log
+
+class MealViewController: UIViewController, UITextFieldDelegate, UIImagePickerControllerDelegate, UINavigationControllerDelegate {
+    
+    //MARK: Properties
+    @IBOutlet weak var nameTextField: UITextField!
+    @IBOutlet weak var photoImageView: UIImageView!
+    @IBOutlet weak var ratingControl: RatingControl!
+    @IBOutlet weak var saveButton: UIBarButtonItem!
+    @IBOutlet weak var scrollView: UIScrollView!
+    @IBOutlet weak var size: UITextField!
+    @IBOutlet weak var imageViewBottomConstraint: NSLayoutConstraint!
+    @IBOutlet weak var imageViewLeadingConstraint: NSLayoutConstraint!
+    @IBOutlet weak var imageViewTopConstraint: NSLayoutConstraint!
+    @IBOutlet weak var imageViewTrailingConstraint: NSLayoutConstraint!
+    
+    /*
+     This value is either passed by `MealTableViewController` in `prepare(for:sender:)`
+     or constructed as part of adding a new meal.
+     */
+    var location = CGPoint(x: 0, y: 0)
+    
+    var meal: Meal?
+    
+    override func viewDidLoad() {
+        super.viewDidLoad()
+        
+        // Handle the text field’s user input through delegate callbacks.
+        nameTextField.delegate = self
+        size.delegate = self
+        
+        // Set up views if editing an existing Meal.
+        if let meal = meal {
+            navigationItem.title = meal.name
+            nameTextField.text = meal.name
+            photoImageView.image = meal.photo
+            ratingControl.rating = meal.rating
+            size.text = meal.size
+            
+        }
+        
+        photoImageView.bounds.size = (photoImageView.image?.size)!
+        
+        //scrollView.addSubview(photoImageView)
+        //view.addSubview(scrollView)
+        //print("viewDidLoad photoImageview.image.size: \(photoImageView.image?.size)")
+        print("viewDidLoad photoImageView.frame.width: \(photoImageView.frame.width) height: \(photoImageView.frame.height) view h: \(view.frame.height) w: \(view.frame.width) scrollview.frame h: \(scrollView.frame.height) w: \(scrollView.frame.width)")
+        
+        // Enable the Save button only if the text field has a valid Meal name.
+        updateSaveButtonState()
+    }
+    
+    //MARK: UITextFieldDelegate
+    
+    func textFieldDidBeginEditing(_ textField: UITextField) {
+        // Disable the Save button while editing.
+        saveButton.isEnabled = false
+    }
+    
+    func textFieldShouldReturn(_ textField: UITextField) -> Bool {
+        // Hide the keyboard.
+        textField.resignFirstResponder()
+        return true
+    }
+    
+    func textFieldDidEndEditing(_ textField: UITextField) {
+        updateSaveButtonState()
+        //navigationItem.title = textField.text
+        navigationItem.title = nameTextField.text!
+    }
+    
+    //MARK: UIImagePickerControllerDelegate
+    func imagePickerControllerDidCancel(_ picker: UIImagePickerController) {
+        // Dismiss the picker if the user canceled.
+        dismiss(animated: true, completion: nil)
+    }
+    
+    func imagePickerController(_ picker: UIImagePickerController, didFinishPickingMediaWithInfo info: [String : Any]) {
+        
+        // The info dictionary may contain multiple representations of the image. You want to use the original.
+        guard let selectedImage = info[UIImagePickerControllerOriginalImage] as? UIImage else {
+            fatalError("Expected a dictionary containing an image, but was provided the following: \(info)")
+        }
+        
+        // Set photoImageView to display the selected image.
+        //print("imagePickerController selectedImage.size: \(selectedImage.size)")
+        //selectedImage.contentMode = UIViewContentModeScaleAspectFit
+        //let imageView = UIImageView(image: selectedImage)
+        let imageView = UIImageView(image: photoImageView.image)
+        imageView.contentMode = UIViewContentMode.scaleAspectFit
+        imageView.bounds.size = (photoImageView.image?.size)!
+        
+        //imageView.frame = CGRect(x: 186, y: 240, width: 100, height: 200)
+        //imageView.center = photoImageView.center
+        imageView.frame = CGRect(x: 687, y: 1516, width: 1650, height: 1000)
+        //imageView.addSubview(self.view)
+        photoImageView.image = selectedImage
+        photoImageView.bounds.size = (photoImageView.image?.size)!
+        
+        photoImageView.addSubview(imageView)
+        
+        
+        print("imagePickerController photoImageview.image.size: \(photoImageView.image?.size) imageView.size: \(imageView.image?.size)")
+        
+        //print("imagePickerController photoImageView.center: \(photoImageView.center) photoImageView.subview[0].center: \(photoImageView.subviews[0].center)")
+        
+        //self.view.addSubview(imagieView)
+        
+        // Dismiss the picker.
+        dismiss(animated: true, completion: nil)
+    }
+    
+    //MARK: Navigation
+    
+    @IBAction func cancel(_ sender: UIBarButtonItem) {
+        // Depending on style of presentation (modal or push presentation), this view controller needs to be dismissed in two different ways.
+        let isPresentingInAddMealMode = presentingViewController is UINavigationController
+        
+        if isPresentingInAddMealMode {
+            dismiss(animated: true, completion: nil)
+        }
+        else if let owningNavigationController = navigationController{
+            owningNavigationController.popViewController(animated: true)
+        }
+        else {
+            fatalError("The MealViewController is not inside a navigation controller.")
+        }
+    }
+    
+    // This method lets you configure a view controller before it's presented.
+    override func prepare(for segue: UIStoryboardSegue, sender: Any?) {
+        
+        super.prepare(for: segue, sender: sender)
+        
+        // Configure the destination view controller only when the save button is pressed.
+        guard let button = sender as? UIBarButtonItem, button === saveButton else {
+            os_log("The save button was not pressed, cancelling", log: OSLog.default, type: .debug)
+            return
+        }
+        
+        let name = nameTextField.text ?? ""
+        let photo = photoImageView.image
+        let rating = ratingControl.rating
+        let size = self.size.text ?? ""
+        
+        
+        // Set the meal to be passed to MealTableViewController after the unwind segue.
+        meal = Meal(name: name, photo: photo, rating: rating, size: size)
+    }
+    
+    //MARK: Actions
+    @IBAction func selectImageFromCamera(_ sender: UIBarButtonItem) {
+        // Hide the keyboard.
+        nameTextField.resignFirstResponder()
+        
+        // UIImagePickerController is a view controller that lets a user pick media from their photo library.
+        let cimagePickerController = UIImagePickerController()
+        
+        // Only allow photos to be taken.
+        //imagePickerController.sourceType = .photoLibrary
+        cimagePickerController.sourceType = .camera
+        cimagePickerController.allowsEditing = true
+        cimagePickerController.showsCameraControls = true
+        cimagePickerController.mediaTypes = UIImagePickerController.availableMediaTypes(for: .camera)!
+        // Make sure ViewController is notified when the user picks an image.
+        cimagePickerController.delegate = self
+        present(cimagePickerController, animated: true, completion: nil)    }
+    
+    @IBAction func selectImageFromPhotoLibrary(_ sender: UITapGestureRecognizer) {
+        
+        // Hide the keyboard.
+        nameTextField.resignFirstResponder()
+        
+        // UIImagePickerController is a view controller that lets a user pick media from their photo library.
+        let imagePickerController = UIImagePickerController()
+        
+        // Only allow photos to be picked, not taken.
+        imagePickerController.sourceType = .photoLibrary
+        //imagePickerController.sourceType = .camera
+        //imagePickerController.allowsEditing = true
+        //imagePickerController.showsCameraControls = true
+        //imagePickerController.mediaTypes = UIImagePickerController.availableMediaTypes(for: .camera)!
+        // Make sure ViewController is notified when the user picks an image.
+        imagePickerController.delegate = self
+        present(imagePickerController, animated: true, completion: nil)
+    }
+    
+    //MARK: Private Methods
+    
+    private func updateSaveButtonState() {
+        // Disable the Save button if the text field is empty.
+        let text = nameTextField.text ?? ""
+        saveButton.isEnabled = !text.isEmpty
+    }
+    
+    
+    fileprivate func updateMinZoomScaleForSize(_ size: CGSize) {
+        //print("updateZoomScrollForSize Before scrollView.bounds.size: \(size) photoImageView.bounds.size: \(photoImageView.bounds.size)")
+        
+        let widthScale = size.width / photoImageView.bounds.width
+        let heightScale = size.height / photoImageView.bounds.height
+        let minScale = min(widthScale, heightScale)
+        
+        scrollView.minimumZoomScale = minScale
+        
+        scrollView.zoomScale = minScale
+        //print("updateZoomScrollForSize After ScrollView.bounds.size: \(size) photoImageView.bounds.size: \(photoImageView.bounds.size) minScale: \(minScale)")
+        //print("updateZoomScrollForSize After scrollView.bounds.size: \(size) photoImageView.frame.size: \(photoImageView.frame.size) minScale: \(minScale)")
+        
+    }
+    
+    override func viewDidLayoutSubviews() {
+        super.viewDidLayoutSubviews()
+        // 4
+        //updateMinZoomScaleForSize(view.bounds.size)
+        updateMinZoomScaleForSize(scrollView.bounds.size)
+    }
+    
+    fileprivate func updateConstraintsForSize(_ size: CGSize) {
+        
+        //print("updateConstraintForSize before: scrollView.bounds.size: \(size) photoImageView.frame.size: \(photoImageView.frame.size)")
+        let yOffset = max(0, (size.height - photoImageView.frame.height) / 2)
+        //yOffset = 0
+        imageViewTopConstraint.constant = yOffset
+        imageViewBottomConstraint.constant = yOffset
+        
+        let xOffset = max(0, (size.width - photoImageView.frame.width) / 2)
+        
+        imageViewLeadingConstraint.constant = xOffset
+        imageViewTrailingConstraint.constant = xOffset
+        
+        //print("updateConstraintForSize After TopConstraint: \(imageViewTopConstraint.constant) Bottom: \(imageViewBottomConstraint.constant) Lead: \(imageViewLeadingConstraint.constant) Trail: \(imageViewTrailingConstraint.constant)")
+        
+        
+        view.layoutIfNeeded()
+    }
+    
+    //Move Artwork Image
+    @IBAction func moveArtWork(_ sender: UIPanGestureRecognizer) {
+        if ((sender.state != UIGestureRecognizerState.ended) && (sender.state != UIGestureRecognizerState.failed) && (sender.view?.subviews.count != 0)) {
+         
+            //print("sender.view?.subviews.count: \(sender.view?.subviews.count)")
+         
+            //print("moveArtWork view.subviews[0].location: \(sender.location(in: sender.view?.subviews[0])) view.location: \(sender.location(in: sender.view)) superview.location \(sender.location(in: sender.view?.superview))")
+            sender.view?.subviews[0].center = sender.location(in: sender.view)
+        }
+        
+    }
+    /* use moveArtWork
+     override func touchesBegan(_ touches: Set<UITouch>, with event: UIEvent?) {
+     
+     print("touchesBegin")
+     let touch: UITouch! = touches.first! as UITouch
+     location = touch.location(in: photoImageView)
+     photoImageView.subviews[0].center = location
+     
+     }
+     override func touchesMoved(_ touches: Set<UITouch>, with event: UIEvent?) {
+     print("touchesMoved")
+     let touch: UITouch! = touches.first! as UITouch
+     location = touch.location(in: photoImageView)
+     photoImageView.subviews[0].center = location
+     
+     }
+     */
+    
+}
+
+extension MealViewController: UIScrollViewDelegate {
+    func viewForZooming(in scrollView: UIScrollView) -> UIView? {
+        // 1
+        return photoImageView
+    }
+    
+    func scrollViewDidZoom(_ scrollView: UIScrollView) {
+        updateConstraintsForSize(scrollView.bounds.size)  // 4
+    }
+    
+}
+
